539526bf94fbf5e40339e71c9c714861f14c99f3
Stanislaw Swierc <stanislaw.swierc@gmail.com>
2018-10-26 22:12:30 -0700

    Create placeholder for interaction model

---
+++ src/WattTime.Agent/Interaction/InteractionModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WattTime.Agent.Interaction
+{
+    public static class InteractionModel
+    {
+        public static class Intent
+        {
+            public const string Stop = "AMAZON.Stop";
+        }
+    }
+}
